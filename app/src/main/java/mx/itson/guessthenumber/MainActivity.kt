f9b2cfebf089880ae865c74e153d3186c13c8b09
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2021-03-15 21:39:52 -0700

    Clean code

--- app/src/main/java/mx/itson/guessthenumber/MainActivity.kt
+++ app/src/main/java/mx/itson/guessthenumber/MainActivity.kt
@@ -8,64 +8,64 @@ import android.widget.TextView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    var minValue = 0;
-    var maxValue = 0;
-    var num: Int = 0;
-    var won = false;
+    var minValue = 0
+    var maxValue = 0
+    var num: Int = 0
+    var won = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val guessings: TextView = findViewById(R.id.guessings);
-        val down: Button = findViewById(R.id.down);
-        val up: Button = findViewById(R.id.up);
-        val generate: Button = findViewById(R.id.generate);
-        val guessed: Button = findViewById(R.id.guessed);
+        val guessings: TextView = findViewById(R.id.guessings)
+        val down: Button = findViewById(R.id.down)
+        val up: Button = findViewById(R.id.up)
+        val generate: Button = findViewById(R.id.generate)
+        val guessed: Button = findViewById(R.id.guessed)
 
         generate.setOnClickListener {
-            num = Random.nextInt(minValue, maxValue);
-            guessings.setText(num.toString());
-            generate.visibility = View.INVISIBLE;
-            guessed.visibility = View.VISIBLE;
+            num = Random.nextInt(minValue, maxValue)
+            guessings.text = num.toString()
+            generate.visibility = View.INVISIBLE
+            guessed.visibility = View.VISIBLE
 
         }
         up.setOnClickListener{
-            minValue = num;
+            minValue = num
             if (checkingLimits()) {
-                num = Random.nextInt(minValue, maxValue);
-                guessings.setText(num.toString());
+                num = Random.nextInt(minValue, maxValue)
+                guessings.text = num.toString()
             }else{
-                guessings.setText("This can't be possible, you win");
+                guessings.text = "This can't be possible, you win"
             }
         }
         down.setOnClickListener {
-            maxValue = num;
+            maxValue = num
             if (checkingLimits()) {
-                num = Random.nextInt(minValue, maxValue);
-                guessings.setText(num.toString());
+                num = Random.nextInt(minValue, maxValue)
+                guessings.text = num.toString()
             }else{
-                guessings.setText("This can't be possible, you win");
+                guessings.text = "This can't be possible, you win"
             }
         }
         guessed.setOnClickListener {
             if(!won) {
-                guessings.setText("I guessed it! your number is: " + num);
-                guessed.setText("Play again");
-                won = true;
+                guessings.text = "I guessed it! your number is: " + num
+                guessed.text = "Play again"
+                won = true
             }else{
-                generate.visibility = View.VISIBLE;
-                guessings.setText("Tap on generate to start");
-                guessed.visibility = View.GONE;
-                resetValues();
+                generate.visibility = View.VISIBLE
+                guessings.text = "Tap on generate to start"
+                guessed.visibility = View.GONE
+                resetValues()
             }
         }
     }
     fun resetValues(){
-        minValue = 0;
-        maxValue = 100;
-        num = 0;
-        won = false;
+        minValue = 0
+        maxValue = 100
+        num = 0
+        won = false
     }
     fun checkingLimits():Boolean{
-        return minValue != maxValue;
+        return minValue != maxValue
     }
 }
